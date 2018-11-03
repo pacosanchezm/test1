7bffefdac52f5d2dc2b63197dbb01ab2e350b4c7
Paco Sánchez M <pacosanchezm@MacBook-Pro-3.local>
2018-11-03 07:47:02 -0600

    200

--- client2/iniciosusuariosdata.jsx
+++ client2/iniciosusuariosdata.jsx
@@ -448,7 +448,7 @@ export default class Usuario extends React.PureComponent {
           >
             <MiTextArea
               css={{
-                width: "400px",
+                width: "200px",
                 height: "144px",
                 align: "top",
                 padding: "5px",
